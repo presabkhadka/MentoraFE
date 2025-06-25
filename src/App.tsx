60df0028fbe280ef1db108dc7b770583faeae5c6
Presab <presabkhadka30@gmail.com>
2025-06-25 12:07:04 +0545

    updt - vercel.json file for reloading error

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,38 @@ import Signup from "./pages/auth/Signup";
 import { Toaster } from "react-hot-toast";
 import X from "./pages/view/X";
 import Youtube from "./pages/view/Youtube";
+import Try from "./pages/view/Try";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 export default function App() {
+  const queryClient = new QueryClient();
   return (
     <>
-      <RouterProvider
-        router={createBrowserRouter([
-          { path: "/", element: <Login /> },
-          { path: "/signup", element: <Signup /> },
-          {
-            path: "/user",
-            element: <AuthGuard />,
-            children: [
-              { path: "/user/home", element: <Home /> },
-              { path: "/user/x", element: <X /> },
-              { path: "/user/youtube", element: <Youtube /> },
-            ],
-          },
-        ])}
-      ></RouterProvider>
-      <Toaster position="bottom-right" />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider
+          router={createBrowserRouter([
+            { path: "/", element: <Login /> },
+            { path: "/try", element: <Try /> },
+            { path: "/signup", element: <Signup /> },
+            {
+              path: "/user",
+              element: <AuthGuard />,
+              children: [
+                { path: "/user/home", element: <Home /> },
+                { path: "/user/x", element: <X /> },
+                { path: "/user/youtube", element: <Youtube /> },
+              ],
+            },
+          ])}
+        ></RouterProvider>
+        <Toaster position="bottom-right" />
+      </QueryClientProvider>
     </>
   );
 }
